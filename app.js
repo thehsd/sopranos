a2e58ab68dfe76967dea33e172db78341fd21f22
alireza hosseini <hosseinialireza485@gmail.com>
2025-09-28 16:53:43 +0330

    Initial commit: Add HTML structure, CSS styles, and JavaScript for menu functionality, including menu data and layout for a restaurant website.

---
+++ app.js
@@ -0,0 +1,185 @@
+const menuData = [
+    {
+        title: 'Antipasti',
+        items: [
+            {
+                title: 'Tagliere affettati e formaggi',
+                subHeader: '',
+                price: 16,
+            },
+            {
+                title: 'Bufala Prosciutto',
+                subHeader: '',
+                price: 12,
+            },
+
+            {
+                title: 'Caprese',
+                subHeader: '',
+                price: 12,
+            },
+
+            {
+                title: 'Caponatina Agrodolce',
+                subHeader: '',
+                price: 9,
+            },
+
+
+            {
+                title: 'Caponatina Agrodolce',
+                subHeader: '(Berenjena salio)',
+                price: 9,
+            },
+
+
+
+
+        ]
+    },
+    {
+        title: 'Primi',
+        items: [
+            {
+                title: 'Spaghettini alle Vongole',
+                subHeader: '',
+                price: 14,
+            },
+
+            {
+                title: 'Bombolotti Cacioe Pepe',
+                subHeader: '',
+                price: 14,
+            },
+
+
+            {
+                title: 'Lasagna alla Bolognese',
+                subHeader: '',
+                price: 11,
+            },
+
+
+            {
+                title: 'Trofie al pesto',
+                subHeader: '',
+                price: 9,
+            },
+
+
+            {
+                title: 'Carbonara',
+                subHeader: '',
+                price: 12,
+            },
+
+
+            {
+                title: 'Amatriclana',
+                subHeader: '',
+                price: 12,
+            },
+
+        ]
+    },
+    {
+        title: 'dolci',
+        items: [
+            {
+                title: 'Tiramisu classico',
+                subHeader: '',
+                price: 5,
+            },
+
+
+            {
+                title: 'Tiramisu Pistochio',
+                subHeader: '',
+                price: 6,
+            },
+
+
+            {
+                title: 'Gelato Vaniglia Caffe',
+                subHeader: '',
+                price: 6,
+            },
+        ]
+    },
+    {
+        title: 'secondi',
+        items: [
+            {
+                title: 'Pollo alla cacciatora',
+                subHeader: '',
+                price: 16,
+            },
+
+
+            {
+                title: 'Polpette al sugo con pecorino',
+                subHeader: '',
+                price: 14,
+            },
+
+
+            {
+                title: 'Ossobuco alla Romana',
+                subHeader: '',
+                price: 18,
+            },
+
+
+            {
+                title: 'Salmone al pistachio',
+                subHeader: '',
+                price: 20,
+            },
+
+
+            {
+                title: 'Straccio Romano',
+                subHeader: '(Carne ajo y pimienta)',
+                price: 22,
+            },
+
+
+        ]
+    }
+]
+const menuItemsSection = document.querySelector('.menu-items-section');
+
+menuData.forEach(item => {
+    const menuItem = document.createElement('div');
+    menuItem.classList.add('menu-items-group');
+    menuItem.innerHTML = `
+  
+                            <div class="menu-items-header">
+                                <h3>
+                                    ${item.title.toUpperCase()}
+                                </h3>
+                            </div>
+                            <div class="menu-items-content">
+                                ${item.items.map(item => `
+                                    <div class="menu-item">
+                                        <div class="menu-item-price-top">
+                                          <span>€</span>  
+                                           <span>
+                                                ${item.price}
+                                            </span>
+                                        </div>
+                                        <div class="menu-item-info">
+
+                                        <p class="menu-item-title">
+                                            ${item.title.toUpperCase()}
+                                        </p>
+                                        <span class="menu-item-sub-header">
+                                            ${item.subHeader.toUpperCase()}
+                                        </span>
+                                    </div>
+                                    </div>
+                                `).join('')}
+                            </div>
+                        `;
+    menuItemsSection.appendChild(menuItem);
+});

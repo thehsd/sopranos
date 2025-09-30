13e91c731b2ca226aceb4249425bcc64dc744f72
alireza hosseini <hosseinialireza485@gmail.com>
2025-09-30 20:17:52 +0330

    feat: add COCKTAIL into menu

--- app.js
+++ app.js
@@ -145,6 +145,63 @@ const menuData = [
 
 
         ]
+    },
+    {
+        title: 'Cocktail',
+        items: [
+            {
+                title: 'Apérol spritz',
+                subHeader: '',
+                price: 5,
+            },
+            {
+                title: 'Campari spritz',
+                subHeader: '',
+                price: 5,
+            },
+            {
+                title: 'Negroni gin',
+                subHeader: '',
+                price: 5,
+            },
+
+            {
+                title: 'Vodka Lemon',
+                subHeader: '',
+                price: 5,
+            },
+            {
+                title: 'Gin lemon',
+                subHeader: '',
+                price: 5,
+            },
+            {
+                title: 'Vodka menta e limone',
+                subHeader: '',
+                price: 0,
+            },
+            {
+                title: 'Dry martini',
+                subHeader: '',
+                price: 0,
+            },
+            {
+                title: 'Cuba libre',
+                subHeader: '',
+                price: 0,
+            },
+            {
+                title: 'Cocktail più pinsa',
+                subHeader: '',
+                price: 12,
+            },
+            {
+                title: 'Gli altri cocktail',
+                subHeader: '',
+                price: 8,
+            }
+
+        ]
     }
 ]
 const menuItemsSection = document.querySelector('.menu-items-section');
@@ -153,33 +210,31 @@ menuData.forEach(item => {
     const menuItem = document.createElement('div');
     menuItem.classList.add('menu-items-group');
     menuItem.innerHTML = `
-  
-                            <div class="menu-items-header">
-                                <h3>
-                                    ${item.title.toUpperCase()}
-                                </h3>
-                            </div>
-                            <div class="menu-items-content">
-                                ${item.items.map(item => `
-                                    <div class="menu-item">
-                                        <div class="menu-item-price-top">
-                                          <span>€</span>  
-                                           <span>
-                                                ${item.price}
-                                            </span>
-                                        </div>
-                                        <div class="menu-item-info">
-
-                                        <p class="menu-item-title">
-                                            ${item.title.toUpperCase()}
-                                        </p>
-                                        <span class="menu-item-sub-header">
-                                            ${item.subHeader.toUpperCase()}
-                                        </span>
-                                    </div>
-                                    </div>
-                                `).join('')}
-                            </div>
-                        `;
+        <div class="menu-items-header">
+            <h3>
+                ${item.title.toUpperCase()}
+            </h3>
+        </div>
+        <div class="menu-items-content">
+            ${item.items.map(subItem => `
+                <div class="menu-item">
+                <div class="menu-item-price-top">
+                ${subItem.price > 0 ? `
+                            <span>€</span>
+                            <span>${subItem.price}</span>
+                            ` : '-'}
+                        </div>
+                    <div class="menu-item-info">
+                        <p class="menu-item-title">
+                            ${subItem.title.toUpperCase()}
+                        </p>
+                        <span class="menu-item-sub-header">
+                            ${subItem.subHeader.toUpperCase()}
+                        </span>
+                    </div>
+                </div>
+            `).join('')}
+        </div>
+    `;
     menuItemsSection.appendChild(menuItem);
 });
